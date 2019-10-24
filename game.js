6b3e677a013bc76b20d506b3a21ceff23fd04505
Keller <kjmikkelson05@gmail.com>
2019-10-24 10:23:09 -0700

    Added player var
    Just basic player with corresponding data inside it

--- game.js
+++ game.js
@@ -5,10 +5,22 @@ gameport.appendChild( renderer.view );
 
 var stage = new PIXI.Container();
 
+var	player = {//player's metadata
+		x: 200,
+		y: 100,
+		speed: 3,
+		xVel: 0,
+		yVel: 0,
+		onGround: false,
+		isJumping: false		
+	};
+
 function animate()
 	{
 	requestAnimationFrame( animate );
 	renderer.render( stage ); 
 	}
+	
+
 
 animate(); 
