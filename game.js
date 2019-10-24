3b49a13023d61379dbf2bcd9f8ca38c92ea014dc
Joshus Tenakhongva <jtt75@nau.edu>
2019-10-24 10:09:08 -0700

    fixed gameport

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var gameport = document.getElementById( "gameport" );
 var renderer = PIXI.autoDetectRenderer({ width: 800, height: 600, backgroundColor: 0x6ac48a });
-												{ width: 800, height: 600, backgroundColor: 0x6ac48a });
+
 gameport.appendChild( renderer.view );
 
 var stage = new PIXI.Container();
