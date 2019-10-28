58fa760449bdad188c388683f8288324d6b93101
am009168 <43388560+am009168@users.noreply.github.com>
2019-10-27 22:37:07 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
+var GAME_WIDTH = 720;
+var GAME_HEIGHT = 400;
+var GAME_SCALE = 4;
+
 var gameport = document.getElementById( "gameport" );
-var renderer = PIXI.autoDetectRenderer({ width: 800, height: 600, backgroundColor: 0x6ac48a });
+var renderer = PIXI.autoDetectRenderer({ GAME_WIDTH, GAME_HEIGHT, backgroundColor: 0x6ac48a });
 
 gameport.appendChild( renderer.view );
 
@@ -7,11 +11,24 @@ gameport.appendChild( renderer.view );
 Different containers for different menus
 */
 var stage = new PIXI.Container();
+stage.scale.x = GAME_SCALE;
+stage.scale.y = GAME_SCALE;
 
 var titleScreen = new PIXI.Container();
+titleScreen.scale.x = GAME_SCALE;
+titleScreen.scale.y = GAME_SCALE;
+
 var gameScreen = new PIXI.Container();
+gameScreen.scale.x = GAME_SCALE;
+gameScreen.scale.y = GAME_SCALE;
+
 var creditsScreen = new PIXI.Container();
+creditsScreen.scale.x = GAME_SCALE;
+creditsScreen.scale.y = GAME_SCALE;
+
 var tutorialScreen = new PIXI.Container();
+tutorialScreen.scale.x = GAME_SCALE;
+tutorialScreen.scale.y = GAME_SCALE;
 
 stage.addChild( titleScreen );
 
@@ -50,7 +67,15 @@ var backButton = new PIXI.Sprite( PIXI.Texture.fromImage( "backButton.png" ));
 backButton.interactive = true;
 backButton.on( 'mousedown', backButtonClickHandler );
 
-var	player = {//player's metadata
+var MOVE_LEFT = 1;
+var MOVE_RIGHT = 2;
+var MOVE_UP = 3;
+var MOVE_DOWN = 4;
+var MOVE_NONE = 0;
+
+var world;
+var player;
+player = {//player's metadata
 		x: 200,
 		y: 100,
 		speed: 3,
@@ -60,16 +85,49 @@ var	player = {//player's metadata
 		isJumping: false
 	};
 
+PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
 
+PIXI.loader
+  .add('map_json', 'testroom.json')
+  .add('tileset', 'tileset.png')
+  .add('blob', 'player_character.png')
+  .load(ready);
 
-function animate()
-	{
-	requestAnimationFrame( animate );
-	renderer.render( stage );
-	}
+function ready() 
+{
+  var tu = new TileUtilities(PIXI);
+  world = tu.makeTiledWorld("map_json", "tileset.png");
+  stage.addChild(world);
+  
+  var blob = world.getObject("blob");
+  
+  player = new PIXI.Sprite(PIXI.loader.resources.blob.texture);
+  player.x = blob.x;
+  player.y = blob.y;
+  player.anchor.x = 0.0;
+  player.anchor.y = 1.0;
+
+  // Find the entity layer
+  var entity_layer = world.getObject("Entities");
+  entity_layer.addChild(player);
+ var entity_layer = world.getObject("Entities");
+  entity_layer.addChild(player);
+
+  player.direction = MOVE_NONE;
+  player.moving = false;
+  animate();
+}
+
+function animate(timestamp)
+{
+	requestAnimationFrame(animate);
+	update_camera();
+	renderer.render(stage);
+ }
 
 initializeTitleScreen();
 animate();
+
 
 /*
 * Desc: Initializes the opening title screen for the game
@@ -107,8 +165,7 @@ function startButtonClickHandler( e )
 	stage.removeChild( titleScreen );
   // Add the container that holds the main game to the stage
 	stage.addChild( gameScreen );
-
-
+	stage.addChild(world);
 	renderer.backgroundColor = 0xffb18a;
   gameScreen.addChild( backButton );
 	}
@@ -171,3 +228,13 @@ function backButtonClickHandler( e )
 
 	renderer.backgroundColor = 0x6ac48a;
 	}
+/*
+*	Moves the camera relative to the player's position
+*/	
+function update_camera() {
+  stage.x = -player.x*GAME_SCALE + GAME_WIDTH/2 - player.width/2*GAME_SCALE;
+  stage.y = -player.y*GAME_SCALE + GAME_HEIGHT/2 + player.height/2*GAME_SCALE;
+  stage.x = -Math.max(0, Math.min(world.worldWidth*GAME_SCALE - GAME_WIDTH, -stage.x));
+  stage.y = -Math.max(0, Math.min(world.worldHeight*GAME_SCALE - GAME_HEIGHT, -stage.y));
+}
+
